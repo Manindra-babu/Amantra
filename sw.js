8c755214f6eb34e3622115f3acfdd09c68f8a187
Himanth Reddy <himanth421@gmail.com>
2025-12-21 20:43:36 +0530

    Update PWA assets: icons, manifest, service worker

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'amantra-v1';
+const ASSETS_TO_CACHE = [
+    '/',
+    '/dashboard.html',
+    '/dashboard.css',
+    '/calendar.css',
+    '/dashboard.js',
+    '/firebase-config.js',
+    '/icon.svg',
+    '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(
+                keyList.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+});
